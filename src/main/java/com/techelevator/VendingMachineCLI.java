2b2e772fbc993604a3088ddf0a96ade5d0df5509
Christopher Guerrero <chris.d.guerrero@gmail.com>
2023-07-27 13:58:41 -0400

    created VendingMachineItem Abstract Class

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -13,6 +13,7 @@ public class VendingMachineCLI {
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
+
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/techelevator/VendingMachineItem.java
@@ -0,0 +1,35 @@
+package com.techelevator;
+
+public abstract class VendingMachineItem {
+    private String name;
+    private double price;
+    private int quantity;
+
+
+    public VendingMachineItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        quantity = 5;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "VendingMachineItem{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
