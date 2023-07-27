c434c597ec68984256c40d84745d89cfafdd429a
Sara Tucker <moroccansara@yahoo.com>
2023-07-27 17:37:49 -0400

    menu class

---
+++ src/main/java/com/techelevator/Menu.java
@@ -0,0 +1,37 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class Menu {
+    private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+    private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+
+    public void mainMenu() {
+        System.out.println("(1) Display Vending Machine Items");
+        System.out.println("(2) Purchase");
+        System.out.println(" (3) Exit");
+
+    }
+    public void purchaseMenu () {
+        System.out.println(" Current money provided: $ ");
+       System.out.println();
+        System.out.println("(1) Feed Money");
+        System.out.println("(2) Select Product");
+        System.out.println(" (3) Finish Transaction");
+
+    }
+    public String getUserInputMainMenu () {
+        Scanner userInput = new Scanner(System.in);
+        String choice = userInput.nextLine();
+
+        if (choice.equals( "1"  ) ) {
+            return MAIN_MENU_OPTION_DISPLAY_ITEMS;
+        } else if (choice.equals("2")) {
+            return MAIN_MENU_OPTION_PURCHASE;
+
+        } else {
+            return "Exit";
+        }
+
+    }
+}
--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,41 +1,48 @@
-//package com.techelevator;
-//
-//// Menu is provided to you as a suggested class to handle user input
-//// Build out a menu class to start
-//
-//public class VendingMachineCLI {
-//
-//	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-//	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-//	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
-//
-//	private Menu menu;
-//
-//	public VendingMachineCLI(Menu menu) {
-//		this.menu = menu;
-//
-//	}
-//
-//	public static void main(String[] args) {
-//		// You will need to create a Menu class to handle display.
-//		//Menu menu = new Menu();
-//		VendingMachineCLI cli = new VendingMachineCLI(menu);
-//		cli.run();
-//	}
-//
-//	public void run() {
-//		while (true) {
-//			// Use a method from your Menu class to initialize this value
-//			String choice = "initialize this here";
-//
-//			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-//				// display vending machine items
-//			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-//				// do purchase
-//			}
-//			else{
-//				break;
-//			}
-//		}
-//	}
-//}
+package com.techelevator;
+
+// Menu is provided to you as a suggested class to handle user input
+// Build out a menu class to start
+
+import java.util.Scanner;
+
+public class VendingMachineCLI {
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+
+	private Menu menu;
+
+	public VendingMachineCLI(Menu menu) {
+		this.menu = menu;
+
+	}
+
+	public static void main(String[] args) {
+		// You will need to create a Menu class to handle display.
+		Menu menu = new Menu();
+		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		cli.run();
+	}
+
+	public void run() {
+		while (true) {
+			menu.mainMenu();
+
+
+			// Use a method from your Menu class to initialize this value
+			String choice = menu.getUserInputMainMenu();
+
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				// display vending machine items
+				System.out.println( " Testing option one");
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				System.out.println(" Testing option two" );
+				// do purchase
+			}
+			else{
+				break;
+			}
+		}
+	}
+}
