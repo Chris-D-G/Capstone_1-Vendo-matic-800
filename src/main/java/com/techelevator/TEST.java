1e9238c6b74c39874312109f139aed278955ab08
Sara Tucker <moroccansara@yahoo.com>
2023-07-28 22:52:17 -0400

    log.txt

--- src/main/java/com/techelevator/TEST.java
+++ src/main/java/com/techelevator/TEST.java
@@ -1,23 +1,20 @@
 package com.techelevator;
 
 import com.techelevator.controller.FileReader;
+import com.techelevator.controller.LogWriter;
 import com.techelevator.controller.MoneyController;
 import com.techelevator.model.VendingMachineItem;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 public class TEST {
 
     public static void main(String[] args) {
-        FileReader test = new FileReader();
-        Map<String, VendingMachineItem> testMap = test.makeItemMap("alternate.csv");
-//        System.out.println(testMap);
-//        System.out.println("Item Name: "+testMap.get("A1").getName() + " , Item Price: "+testMap.get("A1").getPrice() );
-//        System.out.println(testMap.get("B2").toString());
 
-        MoneyController wallet = new MoneyController();
-        wallet.addMoney();
-//        System.out.println(wallet.getWallet());
+        LogWriter logWriter = new LogWriter();
+        logWriter.writeToLog( "Feed money", 5.00, 5.00);
 
 
 
--- src/main/java/com/techelevator/controller/LogWriter.java
+++ src/main/java/com/techelevator/controller/LogWriter.java
@@ -1,11 +1,29 @@
 package com.techelevator.controller;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class LogWriter {
 
     //ToDo Method that logs date time, Action, transaction amount and wallet balance
     public void writeToLog(String action, double transactionAmount, double walletBalance ){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
+        LocalDateTime now = LocalDateTime.now();
+        // create a print writer inside of a try - catch
+        try (FileWriter writer = new FileWriter("Log.txt", true);
+        PrintWriter printer = new PrintWriter (writer) ) {
+            //inside the try have it print out the same information to the log
+            printer.println(dtf.format(now) + " " + action + " $" + transactionAmount + " $" + walletBalance);
+
+        } catch ( IOException e) {
+            System.out.println( "Error");
+        }
 
 
 
