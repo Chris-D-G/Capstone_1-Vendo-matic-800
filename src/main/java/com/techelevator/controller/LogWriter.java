971ab701ccbab14a31713c2494df74fd617f55ee
Christopher Guerrero <chris.d.guerrero@gmail.com>
2023-07-28 22:06:20 -0400

    created a LogWriter Class to generate logs

---
+++ src/main/java/com/techelevator/controller/LogWriter.java
@@ -0,0 +1,16 @@
+package com.techelevator.controller;
+
+import java.io.PrintWriter;
+
+public class LogWriter {
+
+    //ToDo Method that logs date time, Action, transaction amount and wallet balance
+    public void writeToLog(String action, double transactionAmount, double walletBalance ){
+
+
+
+    }
+
+
+
+}
